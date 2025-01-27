753305a80745a7fe506add994b5acfbf68fa71ea
Elvin <seyfullazade.elvin570@gmail.com>
2025-01-27 17:41:08 +0400

    yeni branch1 elave edildi

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("ikinci deyishikliyi etdim");
+        System.out.println("branch1 yaradildi ve ucunce deyishiklik edildi");
     }
 }
